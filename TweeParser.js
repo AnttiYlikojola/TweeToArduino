e91754ebabf2c135915bdb075df07869e90af970
AnttiYlikojola <antti.ylikojola@gmail.com>
2018-08-02 10:53:23 +0300

    nested if/elseif/else

--- TweeParser.js
+++ TweeParser.js
@@ -6,7 +6,7 @@
 
         This script creates array of objects from TwineStory Entweedle story format
 
-        Script has 3 main elements
+        Script has 3 main parts
 
         1. Importing story from file and using cheerio library and split method to get every passage to its own element in array
         2. Regexing every statement to its own element in array
@@ -17,15 +17,16 @@
     "use strict";
     const cheerio = require("cheerio");
     const Promise = require("bluebird");
-    const fs = Promise.promisifyAll(require("fs"));
+    const fs = require("fs");
 
     const compileArduinoCode = require("./ArduinoCompiler") //code that makes the C code for arduino
 
-    const storyFileName = "ohjaustarina2.html"; // tweestory
+    const storyFileName = "ohjaustarina1.html"; // tweestory
+    let startPassage;
     let parsedHtml = []
     let htmlString;
     let sets = [];
-
+    let tweeJSON = []; // Final output
     /* 
     Regular expression for finding:
     closed parenthesis (for goto, live, move, set, 
@@ -36,31 +37,26 @@
 
     const regExp = /\(([^)]+)\)|(\[\[(.*?)\]\])|\[([^\]]*)\]|([^ \r\n][^!?\.|(|[\r\n]+[\w!?\.]+)/g;
 
-    const matchEquations = /(?:\$\w+|\d+)?[><+*/-] ?(?: ?(?:\$\w+|\d+) ??[><+*/-]?)*/g;
-    const matchVariables = /\$(.\S*)/g;
     /* 
-        Read html and parse needed content with cheerio module. 
+        Read html and parse twee story with cheerio module. 
     */
-
-    fs.readFileAsync(storyFileName, "utf-8").then(contents => {
+    fs.readFile(storyFileName, "utf-8", function(err, contents){
         const $ = cheerio.load(contents)
         // element with id #output has the content. Everything before ":: UserStylesheet[stylesheet]" is useless
         htmlString = $("#output").contents()["0"].data.split(":: UserStylesheet[stylesheet]")[1];
+        startPassage = $("tw-storydata").attr("startnode");
+        
         main(htmlString);
-    }).catch(e => {
-        main(htmlString);
-        console.log("Found error somewhere in the code " + e);
-    });
+    })
 
     /* 
-    Iterate through Twee format, every passage is separated with ":: " (split)
-    After that we regex every action to its own element in array 
+        Iterate through Twee format, every passage is separated with ":: " (split)
+
     */ 
     const main = (htmlString) => {
 
 
         const passages = htmlString.split(":: ");     // Split from every passage name
-
         for (let passagesIndex = 1; passagesIndex < passages.length; passagesIndex++) {
 
             let onePassage = { passageHeader: "", passageText: [] };
@@ -68,12 +64,71 @@
             //Passage header is on the first line
             onePassage.passageHeader = passages[passagesIndex].split("\n")[0];
             //Passage data is the rest of the string
-            let passageString = passages[passagesIndex].replace(/\n/g, "").substring(onePassage.passageHeader.length);
-            let onePassageText = passageString.match(regExp)
-            onePassage.passageText = onePassageText;
+            let passageString = passages[passagesIndex].replace(/\n|\t/g, "").substring(onePassage.passageHeader.length);
+
+            onePassage.passageText = parseStatements(passageString);
             parsedHtml.push(onePassage);
         }
+        console.log(parsedHtml)
         parsePassages(parsedHtml);
+    }
+    const parseStatements = (passageString) =>{
+
+        let currentString = "";
+        let opened = "";
+        let openedCounter = 0;
+        let closedCounter = 0;
+        let passageText = []
+
+        for(var j = 0; j < passageString.length; j++){
+
+            if(passageString[j] == "[" || passageString[j] == "(" || passageString[j] + passageString[j +1] == "[["){
+                openedCounter++;
+            }else if(passageString[j] + passageString[j +1] == "]]" || passageString[j] == "]" || passageString[j] == ")"){
+                closedCounter++;
+            }
+
+            if(passageString[j] + passageString[j +1] == "[[" && opened.length == 0){
+                opened = "[[";
+                if(currentString.length > 0){
+                    passageText.push(currentString);
+                }
+                currentString = passageString[j] + passageString[j +1];
+                j++;
+            }else if(passageString[j] == "[" && opened.length == 0){
+                opened = "[";
+                if(currentString.length > 0){
+                    passageText.push(currentString);
+                }
+                currentString = passageString[j];
+            }else if(passageString[j] == "(" && opened.length == 0){
+                opened = "(";
+                if(currentString.length > 0){
+                    passageText.push(currentString);
+                }
+                currentString = passageString[j];
+            }else if(passageString[j] + passageString[j +1] == "]]" && opened == "[[" && closedCounter == openedCounter){
+                opened = "";
+                currentString += passageString[j] + passageString[j +1];
+                passageText.push(currentString);
+                currentString = "";
+                j++;
+            }else if(passageString[j] == "]" && opened == "[" && closedCounter == openedCounter){
+                opened = "";
+                currentString += passageString[j];
+                passageText.push(currentString);
+                currentString = "";
+            }else if(passageString[j] == ")" && opened == "(" && closedCounter == openedCounter){
+                opened = "";
+                currentString += passageString[j];
+                passageText.push(currentString);
+                currentString = "";
+            }else{
+                currentString += passageString[j];
+            }
+            
+        }
+        return passageText;
     }
 
     let i = 0; //index of passages
@@ -83,12 +138,9 @@
         Iterating through all passages and passage actions  
     */
     const parsePassages = (parsedHtml) => {
-
-        let tweeJSON = []
-
         for (i = 0; i < parsedHtml.length; i++) {
             let tweeJSONPassage = {
-                header: parsedHtml[i].passageHeader, //passages header
+                header: parsedHtml[i].passageHeader.replace(/ä/g, "\x84").replace(/Ä/g, "\8E").replace(/ö/g, "\x94").replace(/Ö/g, "\x99"), //passages header
                 passage: [] // statements
             }
             for (ii = 0; ii < parsedHtml[i].passageText.length; ii++) {
@@ -103,7 +155,7 @@
         }        
 
         var flags = {};
-        /* Getting first unique variablename for declaration in ArduinoCompiler */
+        /* Getting first unique variablenames for declaration in ArduinoCompiler */
         tweeJSON.uniqueSets = sets.filter(function(entry) {
             if (flags[entry.variableName]) {
                 return false;
@@ -111,23 +163,26 @@
             flags[entry.variableName] = true;
             return true;
         });
+        // check starting passage
+        if(startPassage > 1){
+            tweeJSON.unshift(tweeJSON[startPassage -1]);
+            tweeJSON.splice(startPassage, 1);
+        }
 
+        console.log(JSON.stringify(tweeJSON, null, 3))
         compileArduinoCode(tweeJSON);
     };
     /* 
         Check statement type
         return object with statement type and actions of it 
     */ 
+   
     const sortStatement = (statementType, oneStatement) => {
-
         if (statementType === "(set:") {
             let set = setStatement(oneStatement);
             sets.push({variableName:set.variable, value:set.value});
             return set;
-        } else if (statementType === "(if:") {
-            ii++;
-            return conditionalStatement(oneStatement, parsedHtml[i].passageText[ii]);
-        } else if (statementType === "(else-if:") {
+        } else if (statementType === "(if:" || statementType === "(else-if:") {
             ii++;
             return conditionalStatement(oneStatement, parsedHtml[i].passageText[ii]);
         } else if (statementType === "(else:)") {
@@ -137,6 +192,8 @@
             return liveStatement(oneStatement);
         } else if (statementType === "(move") {
             return moveStatement(oneStatement);
+        } else if (statementType == "(face"){
+            return faceStatement(oneStatement);
         } else if (statementType === "(goto:") {
             return gotoStatement(oneStatement);
         } else if (statementType[0] + statementType[1] === "[[") {
@@ -156,22 +213,37 @@
             transition: statement.split(" ")[1].replace(")", "")
         }
     };
+    const faceStatement = (statement) =>{
+        return {
+            statement: "face",
+            expression: statement.split(" ")[1].replace(")", "")
+        }
+    }
     const conditionalStatement = (statementString, conditionalActions) => {
         let statement = statementString.split(" ")[0].replace(/\(/, "");
-        let condition = statementString.split(statement)[1].replace(/\s\$|/, "").replace(")", "");
+        let condition = statementString.split(statement)[1].replace(/\s\$|/g, "").replace(")", "");
         let conditionalStatementObject = {
             statement: statement,
             condition:condition,
             conditionalActions: []
         }
-        for (let o = 0; o < conditionalActions.slice(1, -1).match(regExp).length; o++) {
-            let statementType = conditionalActions.slice(1, -1).match(regExp)[o].split(" ")[0]
-            let oneStatement = conditionalActions.slice(1, -1).match(regExp)[o];
-            conditionalStatementObject.conditionalActions.push(sortStatement(statementType, oneStatement));
+        let actions = conditionalActions.slice(1, -1).match(regExp);
+        console.log(actions)
+        for(var a = 0; a < actions.length; a++){
+            if(actions[a].split(" ")[0] == "(if:" || actions[a].split(" ")[0] == "(else-if:)"){
+                conditionalStatementObject.conditionalActions.push(conditionalStatement(actions[a], actions[a +1]));
+                a++;
+            }else if(actions[a] == "(else:)"){
+                conditionalStatementObject.conditionalActions.push(elseStatement(actions[a +1]));
+                a++;           
+            }else{
+                conditionalStatementObject.conditionalActions.push(sortStatement(actions[a].split(" ")[0], actions[a]));
+            }
         }
         return conditionalStatementObject;
     };
     const elseStatement = (elseActions) => {
+
         let elseStatementObject = {
             statement: "else",
             conditionalActions: []
@@ -206,25 +278,27 @@
             return {
                 statement: "link",
                 linkText: bracketsRemoved.split("->")[0],
-                link: bracketsRemoved.split("->")[1]
+                link: bracketsRemoved.split("->")[1].replace(/ä/g, "\x84").replace(/Ä/g, "\8E").replace(/ö/g, "\x94").replace(/Ö/g, "\x99")
             }
         } else if (bracketsRemoved.split("<-").length > 1) {
             return {
                 statement: "link",
                 linkText: bracketsRemoved.split("<-")[0],
-                link: bracketsRemoved.split("<-")[1]
+                link: bracketsRemoved.split("<-")[1].replace(/ä/g, "\x84").replace(/Ä/g, "\8E").replace(/ö/g, "\x94").replace(/Ö/g, "\x99")
             }
         } else {
             return {
                 statement: "link",
-                link: bracketsRemoved
+                link: bracketsRemoved.replace(/ä/g, "\x84").replace(/Ä/g, "\8E").replace(/ö/g, "\x94").replace(/Ö/g, "\x99")
             }
         }
     };
     const passageText = (text) => {
         return {
             statement: "passageText",
-            text: text
+            text: text.replace(/ä/g, "\x84").replace(/Ä/g, "\8E").replace(/ö/g, "\x94").replace(/Ö/g, "\x99")
         };
     }
+    
+
 })();
--- humanoid.ino
+++ humanoid.ino
@@ -1,18 +1,24 @@
-#include <Adafruit_GFX.h>    // Core graphics library
+
+#include <Adafruit_GFX.h>
 #include <MCUFRIEND_kbv.h>
 #include "TouchScreen.h"
 #include <Servo.h>
 #include <FiniteStateMachine.h>
-#pragma GCC diagnostic ignored "-Wwrite-strings"
+#include <Wire.h>
+#include "Adafruit_LEDBackpack.h"
 
-#define  BLACK   0x0000
-#define BLUE    0x001F
-#define RED     0xF800
-#define GREEN   0x07E0
-#define CYAN    0x07FF
+#pragma GCC diagnostic ignored "-Wwrite-strings"
+#pragma GCC diagnostic ignored "-Wnarrowing"
+#pragma GCC diagnostic ignored "-Woverflow"
+#pragma GCC diagnostic ignored "-fpermissive"
+#define BLACK 0x0000
+#define BLUE 0x001F
+#define RED 0xF800
+#define GREEN 0x07E0
+#define CYAN 0x07FF
 #define MAGENTA 0xF81F
-#define YELLOW  0xFFE0
-#define WHITE   0xFFFF
+#define YELLOW 0xFFE0
+#define WHITE 0xFFFF
 #define BLUEBAR_MINX 120
 #define BAR_MINY 60
 
@@ -20,13 +26,42 @@
 #define MAXPRESSURE 1000
 
 // These are the pins for the shield!
-#define YP A1  // must be an analog pin, use "An" notation!
-#define XM A2  // must be an analog pin, use "An" notation!
-#define YM 7   // can be a digital pin
-#define XP 6   // can be a digital pin
+#define YP A1 // must be an analog pin, use "An" notation!
+#define XM A2 // must be an analog pin, use "An" notation!
+#define YM 7  // can be a digital pin
+#define XP 6  // can be a digital pin
+
+static const uint8_t PROGMEM
+    smile_bmp[] =
+        {B00111100,
+         B01000010,
+         B10100101,
+         B10000001,
+         B10100101,
+         B10011001,
+         B01000010,
+         B00111100},
+    neutral_bmp[] =
+        {B00111100,
+         B01000010,
+         B10100101,
+         B10000001,
+         B10111101,
+         B10000001,
+         B01000010,
+         B00111100},
+    frown_bmp[] =
+        {B00111100,
+         B01000010,
+         B10100101,
+         B10000001,
+         B10011001,
+         B10100101,
+         B01000010,
+         B00111100};
 
 int servoPos[9] = {90, 90, 90, 90, 90, 90, 90, 90, 90}; // array for servo position tracking
-int servoInitPos[9]; // array for initializing positions
+int servoInitPos[9];                                    // array for initializing positions
 
 // servo declarations
 Servo ankle_left;
@@ -39,32 +74,28 @@ Servo arm_left;
 Servo arm_right;
 Servo head;
 Servo servos[] = {
-  ankle_left, ankle_right,
-  hip_left, hip_right,
-  shoulder_left, shoulder_right,
-  arm_left, arm_right, head
-};
+    ankle_left, ankle_right,
+    hip_left, hip_right,
+    shoulder_left, shoulder_right,
+    arm_left, arm_right, head};
 const int numOfServos = sizeof(servos) / sizeof(*servos);
 
-char * kissa = "koira";
-
-
-// finite state machine declarations
-State state_stepforward(NULL, &on_stepforward_update, &initPosition );
+// finite state machine declarations (enter state, update state, exit state)
+State state_stepforward(NULL, &on_stepforward_update, &initPosition);
 State state_stepbackward(NULL, &on_stepbackward_update, &initPosition);
 State state_donothing(NULL, NULL, NULL);
-State state_wavelefthand(NULL, &on_wavelefthand_update , &initPosition);
-State state_waverighthand(NULL, &on_waverighthand_update , &initPosition);
+State state_wavelefthand(NULL, &on_wavelefthand_update, &initPosition);
+State state_waverighthand(NULL, &on_waverighthand_update, &initPosition);
 State state_dance(NULL, &on_dance_update, &initPosition);
 State state_moonwalk(NULL, &on_moonwalk_update, &initPosition);
-State state_lookaround(NULL, &on_lookaround_update, &initPosition);
-State state_lookforward(NULL, &on_lookforward_update, &initPosition);
-State state_lookleft(NULL, &on_lookleft_update, &initPosition);
-State state_lookright(NULL, &on_lookright_update, &initPosition);
+State state_lookaround(NULL, &on_lookaround_update, NULL);
+State state_lookforward(NULL, &on_lookforward_update, NULL);
+State state_lookleft(NULL, &on_lookleft_update, NULL);
+State state_lookright(NULL, &on_lookright_update, NULL);
 State state_turnleft(NULL, &on_turnleft_update, &initPosition);
 State state_turnright(NULL, &on_turnright_update, &initPosition);
-State state_handsup(NULL, &on_handsup_update, NULL);
-State state_handsdown(NULL, &on_handsdown_update, NULL);
+State state_handsup(NULL, &on_handsup_update, &initPosition);
+State state_handsdown(NULL, &on_handsdown_update, &initPosition);
 FSM fsm = FSM(state_donothing);
 
 bool storyStarted = false;
@@ -72,10 +103,20 @@ bool storyStarted = false;
 TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
 MCUFRIEND_kbv tft;
 
-void setup() {
+Adafruit_8x8matrix matrix = Adafruit_8x8matrix();
+
+void setup()
+{
+
+  matrix.begin(0x70); // pass in the address
+  matrix.setRotation(1);
+
   Serial.begin(9600);
-  kissa = "kala";
-  if (!Serial) delay(5000);
+  if (!Serial)
+  {
+    delay(5000);
+  }
+
   Serial.println(tft.width());
   Serial.println(tft.height());
   tft.reset();
@@ -88,78 +129,84 @@ void setup() {
   tft.setCursor(110, 90);
   tft.print("Loading...");
   memcpy(servoInitPos, servoPos, sizeof(servoInitPos));
-  for (int i = 0; i < numOfServos; i += 1) {
+  for (int i = 0; i < numOfServos; i += 1)
+  {
     servos[i].write(servoInitPos[i]);
     Serial.println(i + 30);
     servos[i].attach(i + 30);
     delay(400);
   }
 
-  for (int i; i < 220; i++) {
+  for (int i; i < 220; i++)
+  {
     tft.fillRect(BAR_MINY - 10, BLUEBAR_MINX, i, 10, RED);
     delay(10);
   }
   tft.reset();
   tft.fillScreen(BLACK);
-  tft.fillRoundRect(60, 90, 200, 40, 8, RED);   //RGB led
+  tft.fillRoundRect(60, 90, 200, 40, 8, RED); //RGB led
   tft.drawRoundRect(60, 90, 200, 40, 8, WHITE);
   tft.setCursor(90, 100);
   tft.print("Aloita tarina");
-
 }
 
 
 
-void initPosition() {
+void initPosition()
+{
   Serial.println("do nothing update");
-  for (int i = 0; i < numOfServos; i += 1) {
+  for (int i = 0; i < numOfServos; i += 1)
+  {
     servos[i].write(servoInitPos[i]);
     delay(300);
   }
 }
-void on_stepforward_update() {
+void on_stepforward_update()
+{
   Serial.println("Step forward update");
-  ankle_left.write(80);  //shift weight to left ankle
+  ankle_left.write(80); //shift weight to left ankle
   ankle_right.write(90);
   delay(200);
-  hip_left.write(70); //left hip
+  hip_left.write(70);  //left hip
   hip_right.write(70); //right hip
   delay(200);
   ankle_left.write(90);  //level ankle
-  ankle_right.write(90);  //right ankle
+  ankle_right.write(90); //right ankle
   delay(200);
   ankle_left.write(100); //shift weight to right ankle
   ankle_right.write(110);
   delay(200);
   hip_left.write(110);  //left hip
-  hip_right.write(110);  //right hip
-  delay(200);
-  ankle_left.write(90);  //level ankle
-  ankle_right.write(90);  //right ankle
-  delay(200);
-}
-void on_stepbackward_update() {
-  Serial.println("Step backward update");
-  ankle_left.write(80);  //shift weight to left ankle
-  ankle_right.write(80);
-  delay(200);
-  hip_left.write(110); //left hip
   hip_right.write(110); //right hip
   delay(200);
   ankle_left.write(90);  //level ankle
-  ankle_right.write(90);  //right ankle
+  ankle_right.write(90); //right ankle
+  delay(200);
+}
+void on_stepbackward_update()
+{
+  Serial.println("Step backward update");
+  ankle_left.write(80); //shift weight to left ankle
+  ankle_right.write(80);
+  delay(200);
+  hip_left.write(110);  //left hip
+  hip_right.write(110); //right hip
+  delay(200);
+  ankle_left.write(90);  //level ankle
+  ankle_right.write(90); //right ankle
   delay(200);
   ankle_left.write(100); //shift weight to right ankle
   ankle_right.write(100);
   delay(200);
   hip_left.write(70);  //left hip
-  hip_right.write(70);  //right hip
+  hip_right.write(70); //right hip
   delay(200);
   ankle_left.write(90);  //level ankle
-  ankle_right.write(90);  //right ankle
+  ankle_right.write(90); //right ankle
   delay(200);
 }
-void on_wavelefthand_update() {
+void on_wavelefthand_update()
+{
   Serial.println("on left wave hand update");
   shoulder_left.write(0);
   delay(400);
@@ -177,7 +224,8 @@ void on_wavelefthand_update() {
   delay(400);
   fsm.exitState();
 }
-void on_waverighthand_update() {
+void on_waverighthand_update()
+{
   Serial.println("on left wave hand update");
   shoulder_right.write(180);
   delay(400);
@@ -195,111 +243,118 @@ void on_waverighthand_update() {
   delay(400);
   fsm.exitState();
 }
-void on_dance_update() {
+void on_dance_update()
+{
   head.write(60);
   arm_left.write(130);
   arm_right.write(50);
   shoulder_right.write(120);
   hip_left.write(100);  //left hip
-  hip_right.write(100);  //right hip
+  hip_right.write(100); //right hip
   delay(200);
   head.write(100);
   shoulder_left.write(10);
   arm_left.write(90);
   arm_right.write(90);
   hip_left.write(70);  //left hip
-  hip_right.write(70);  //right hip
+  hip_right.write(70); //right hip
   delay(500);
   shoulder_right.write(160);
   shoulder_left.write(50);
   head.write(50);
   arm_left.write(90);
   arm_right.write(90);
-  ankle_left.write(70);  //left hip
-  ankle_right.write(110);  //right hip
+  ankle_left.write(70);   //left hip
+  ankle_right.write(110); //right hip
   delay(300);
   head.write(130);
   arm_left.write(130);
   arm_right.write(50);
-  hip_left.write(100);  //left hip
+  hip_left.write(100);   //left hip
   hip_right.write(100);  //right hip
   ankle_left.write(90);  //left hip
-  ankle_right.write(90);  //right hip
+  ankle_right.write(90); //right hip
   delay(500);
   head.write(70);
   shoulder_left.write(90);
   arm_left.write(90);
   arm_right.write(20);
-  hip_left.write(40);  //left hip
-  hip_right.write(60);  //right hip
-  ankle_left.write(50);  //left hip
-  ankle_right.write(120);  //right hip
+  hip_left.write(40);     //left hip
+  hip_right.write(60);    //right hip
+  ankle_left.write(50);   //left hip
+  ankle_right.write(120); //right hip
   delay(200);
   head.write(60);
   arm_left.write(130);
   arm_right.write(50);
   hip_left.write(100);  //left hip
-  hip_right.write(100);  //right hip
+  hip_right.write(100); //right hip
   delay(200);
   head.write(100);
   arm_left.write(90);
   arm_right.write(90);
   shoulder_left.write(59);
   hip_left.write(70);  //left hip
-  hip_right.write(70);  //right hip
+  hip_right.write(70); //right hip
   delay(500);
   head.write(50);
   shoulder_right.write(120);
   arm_left.write(90);
   arm_right.write(90);
-  ankle_left.write(70);  //left hip
-  ankle_right.write(110);  //right hip
+  ankle_left.write(70);   //left hip
+  ankle_right.write(110); //right hip
   delay(300);
   head.write(130);
   shoulder_left.write(90);
   shoulder_right.write(90);
   arm_left.write(130);
   arm_right.write(50);
-  hip_left.write(100);  //left hip
+  hip_left.write(100);   //left hip
   hip_right.write(100);  //right hip
   ankle_left.write(90);  //left hip
-  ankle_right.write(90);  //right hip
+  ankle_right.write(90); //right hip
   delay(500);
   head.write(70);
   arm_left.write(90);
   arm_right.write(20);
-  hip_left.write(40);  //left hip
-  hip_right.write(60);  //right hip
-  ankle_left.write(50);  //left hip
-  ankle_right.write(120);  //right hip
+  hip_left.write(40);     //left hip
+  hip_right.write(60);    //right hip
+  ankle_left.write(50);   //left hip
+  ankle_right.write(120); //right hip
   delay(200);
 }
-void on_moonwalk_update() {
+void on_moonwalk_update()
+{
   Serial.println("on moonwalk update");
 }
-void on_lookaround_update() {
+void on_lookaround_update()
+{
   Serial.println("on look around update");
   head.write(45);
   delay(1000);
   head.write(160);
   delay(1000);
 }
-void on_lookleft_update() {
+void on_lookleft_update()
+{
   Serial.println("on look left update");
   head.write(160);
   delay(1000);
 }
-void on_lookforward_update() {
+void on_lookforward_update()
+{
   Serial.println("on look left update");
   head.write(90);
   delay(1000);
 }
-void on_lookright_update() {
+void on_lookright_update()
+{
   Serial.println("on look right update");
   head.write(45);
   delay(1000);
 }
-void on_turnleft_update() {
+void on_turnleft_update()
+{
   ankle_left.write(70);
   delay(500);
   ankle_right.write(120);
@@ -314,7 +369,8 @@ void on_turnleft_update() {
   delay(500);
   Serial.println("on turn left update");
 }
-void on_turnright_update() {
+void on_turnright_update()
+{
   ankle_right.write(110);
   delay(500);
   ankle_left.write(70);
@@ -329,13 +385,34 @@ void on_turnright_update() {
   delay(500);
   Serial.println("on turn right update");
 }
-void on_handsup_update() {
+void on_handsup_update()
+{
   shoulder_left.write(180);
   shoulder_right.write(180);
   Serial.println("on hands up update");
 }
-void on_handsdown_update() {
+void on_handsdown_update()
+{
   shoulder_left.write(180);
   shoulder_right.write(180);
   Serial.println("on hands up update");
+}
+
+void smilyFace()
+{
+  matrix.clear();
+  matrix.drawBitmap(0, 0, smile_bmp, 8, 8, LED_ON);
+  matrix.writeDisplay();
+}
+void neutralFace()
+{
+  matrix.clear();
+  matrix.drawBitmap(0, 0, neutral_bmp, 8, 8, LED_ON);
+  matrix.writeDisplay();
+}
+void sadFace()
+{
+  matrix.clear();
+  matrix.drawBitmap(0, 0, frown_bmp, 8, 8, LED_ON);
+  matrix.writeDisplay();
 }
